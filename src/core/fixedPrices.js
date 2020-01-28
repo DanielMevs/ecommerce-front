b06ba4373970c81ffa7de0a0eab909c51aeaa762
daniel <dannymevs@hotmail.com>
2020-01-28 08:51:43 -0500

    my first commit

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,33 @@
+export const prices = [
+    {
+        _id: 0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id: 1,
+        name: "$0 to $50",
+        array: [0, 50]
+    },
+    {
+        _id: 2,
+        name: "$50 to $200",
+        array: [50, 200]
+    },
+    {
+        _id: 3,
+        name: "$200 to 500",
+        array: [200, 500]
+    },
+    {
+        _id: 4,
+        name: "$500 to 1000",
+        array: [500, 1000]
+    },
+    {
+        _id: 5,
+        name: "$1000 plus",
+        array: [1000, 10000000]
+    }
+
+]
